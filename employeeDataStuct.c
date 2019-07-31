cdf2023bb2d4e06306d530fd5c6356e3d650cd2a
Jesswin W Varghese <jesswinw@gmail.com>
2019-07-31 22:27:37 +0530

    a simple structure program for taking values of an employee

--- employeeDataStuct.c
+++ employeeDataStuct.c
@@ -2,24 +2,30 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct employee{
+/*This is a basic program which uses a structure to
+  read the details of employee which is name,dob and salary and
+  stores it as well as prints it 
+*/
+
+struct employee{ //employee structure to hold employee data
     char name[50];
     char dob[20];
     int salary;
 };
 
 
-void fgetsCheck(char *parr,int len){
+void fgetsCheck(char *parr,int len){ //to check and clear '\n' if it is the end of the charcter(it happens when fgets is used)
     if(*(parr+(len-1))=='\n')
         *(parr+(len-1))='\0';
 
 }
-void main(){
+void main()
+{
     int salary,leng;
-    struct employee emp;
+    struct employee emp; //emp of employee structure type
     printf("Enter Employee Name:");
     fgets(emp.name,50,stdin);
-    leng=strlen(emp.name);
+    leng=strlen(emp.name); //strlen to check for the end of the string
     fgetsCheck(emp.name,leng);
     printf("Enter his/her dob (dd/mm/yyyy) :");
     fgets(emp.dob,20,stdin);
@@ -29,5 +35,6 @@ void main(){
     scanf("%d",&salary);
     emp.salary=salary;
     
+    /*Printing the values */
     printf("\n\nNAME:%s\nDOB:%s\nSALARY:%d\n",emp.name,emp.dob,emp.salary);
     }
