b902db203da42cbe8daf56478babb84182cf993f
Jesswin W Varghese <jesswinw@gmail.com>
2019-07-31 22:24:47 +0530

    Minor tweaks

--- data_using_structure.c
+++ data_using_structure.c
@@ -1,76 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 struct bio_data{
     char name[50];
-    char age[sizeof(int)];
+    int age;
     char hname[70];
     char city[50];
     char state[50];
     char country[50];
 };
 
+void fgetsCheck(char *parr,int len){ //to check and clear '\n' if it is the end of the charcter(it happens when fgets is used)
+    if(*(parr+(len-1))=='\n')
+        *(parr+(len-1))='\0';
+
+}
+
 int main(){
     int len;
+    char tage[sizeof(int)];//tage for temporarly storing value of age 
     struct bio_data entry;
     printf("Enter your name:");
     fgets(entry.name,50,stdin);
     len=strlen(entry.name);
-    if(entry.name[len-1]=='\n')
-    {
-        entry.name[len-1]='\0';
-    }
+    fgetsCheck(entry.name,len);
     
     
     printf("Enter your age:");
-    fgets(entry.age,sizeof(int),stdin);
-    len=strlen(entry.name);
-    if(entry.age[len-1]=='\n')
-    {
-        entry.age[len-1]='\0';
-    }
+    fgets(tage,sizeof(int),stdin);
+    len=strlen(tage);
+    fgetsCheck(tage,len);
+    entry.age=atoi(tage);//coverts character string tage to integer and stores it to entry.age
 
 
     printf("Enter your house name:");
     fgets(entry.hname,70,stdin);
     len=strlen(entry.hname);
-    if(entry.hname[len-1]=='\n')
-    {
-        entry.hname[len-1]='\0';
-    }
+    fgetsCheck(entry.hname,len);
     
     
     printf("Enter the name of your city:");
     fgets(entry.city,50,stdin);
     len=strlen(entry.city);
-    if(entry.city[len-1]=='\n')
-    {
-        entry.city[len-1]='\0';
-    }
+    fgetsCheck(entry.city,len);
 
     
     printf("Enter the name of the state you are residing in:");
     fgets(entry.state,50,stdin);
     len=strlen(entry.state);
-    if(entry.state[len-1]=='\n')
-    {
-        entry.state[len-1]='\0';
-    }
+    fgetsCheck(entry.state,len);
     
 
     printf("Enter the name of the country you are residing in:");
     fgets(entry.country,50,stdin);
     len=strlen(entry.country);
-    if(entry.country[len-1]=='\n')
-    {
-        entry.country[len-1]='\0';
-    }
+    fgetsCheck(entry.country,len);
     
 
 
     printf("----------DATA ACCESSED FROM STRUCTURE BIO_DATA----------");
     printf("\nYour name:%s",entry.name);
-    printf("\nYour age:%s",entry.age);
+    printf("\nYour age:%d",entry.age);
     printf("\n-----Your ADDRESS-----\n\n%s,\n%s,\n%s,\n%s\n",entry.hname,entry.city,entry.state,entry.country);
 
     return 0;
