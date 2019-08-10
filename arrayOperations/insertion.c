02e2cf233171118152d776a9723cc1c21f523ec0
Jesswin W Varghese <jesswinw@gmail.com>
2019-08-10 14:03:39 +0530

    basic array insertion by the user

--- arrayOperations/insertion.c
+++ arrayOperations/insertion.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 void parray(int *parr,int ls)
 {
     for (int ind = 0;ind<ls;ind++)
     {
         printf("%d\t",*(parr+ind));
     }
-    printf("\n\n");
+    printf("\n");
 }
+
+
 int main()
 {
     int *arr,el,max,lst=0;
@@ -17,19 +21,22 @@ int main()
     scanf("%d",&max);
 
     arr = (int *) malloc(sizeof(int)*max);
+    
     for (int i=0;i<max;i++)
     {
-        printf("Enter the %d element to be inserted:",i);
-        scanf("%d",&el);
+        printf("\nEnter the %dth element for insertion:",i+1);
+        fscanf(stdin,"%d",&el);
         *(arr+i)=el;
-        printf("INSERTED:%d\n\n",el);
+        printf("INSERTED:");
         lst++;
         parray(arr,lst);
     }
 
-    printf("----------All Array elements inserted----------\n\n");
-    printf("Array:\t");
+    printf("\n\n----------All Array elements inserted----------\n\n");
+    printf("ARRAY::::");
     parray(arr,lst);
+    
+    free(arr);
     return 0;
 
 }
